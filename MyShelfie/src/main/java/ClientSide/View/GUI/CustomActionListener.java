9189533d812857829177ba9ffc3eec06cd65a7e7
fgangi <10793520@polimi.it>
2023-05-08 00:40:11 +0200

    first GUI implementation attempt

---
+++ MyShelfie/src/main/java/ClientSide/View/GUI/CustomActionListener.java
@@ -0,0 +1,25 @@
+package ClientSide.View.GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CustomActionListener implements ActionListener {
+
+    String type;
+
+    JLabel label;
+
+    public CustomActionListener(String type, JLabel label) {
+        this.type = type;
+        this.label = label;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (type) {
+            case "create" -> System.out.println("Game created");
+            case "join" -> System.out.println("Game joined");
+        }
+    }
+}
---
+++ MyShelfie/src/main/java/ClientSide/View/GUI/GUI.java
@@ -0,0 +1,55 @@
+package ClientSide.View.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class GUI {
+
+    public static void main(String[] args) { GUI gui = new GUI(); }
+
+    public GUI() {
+
+        JFrame mainFrame = new JFrame();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1140,760);
+        mainFrame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1,2));
+
+        JLabel createL = new JLabel();
+        JLabel joinL = new JLabel();
+
+        JButton createB = new JButton("Create game");
+        createB.addActionListener(new CustomActionListener("create", createL));
+        JButton joinB = new JButton("Join game");
+        joinB.addActionListener(new CustomActionListener("join", joinL));
+        //JLabel usn = new JLabel("Enter username");
+
+        panel.add(createB);
+        panel.add(joinB);
+
+
+        ImageIcon img = new ImageIcon("MyShelfie/src/main/resources/Display_1.jpg");
+        Image scaledImg = img.getImage().getScaledInstance(1140,760,Image.SCALE_SMOOTH);
+        JLabel imgLabel = new JLabel(new ImageIcon(scaledImg));
+        imgLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        imgLabel.setVerticalAlignment(SwingConstants.CENTER);
+
+        //JLayeredPane layeredPane = new JLayeredPane();
+        //layeredPane.add(imgLabel, 0);
+        //layeredPane.add(panel, 1);
+
+        //mainFrame.setLayeredPane(layeredPane);
+
+        mainFrame.add(imgLabel);
+        mainFrame.add(panel);
+
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
+    }
+
+}

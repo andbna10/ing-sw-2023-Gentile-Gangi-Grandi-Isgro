ebcd7ae4971ada83b5dcc30f35dd1ec57246a956
Simone Grandi <126242065+BigSim0@users.noreply.github.com>
2023-05-11 11:40:19 +0200

    print methods

--- MyShelfie/src/main/java/ClientSide/NetworkHandler/PlayerHandler.java
+++ MyShelfie/src/main/java/ClientSide/NetworkHandler/PlayerHandler.java
@@ -16,4 +16,63 @@ public class PlayerHandler implements PlayerViewObserver{
         this.manager = manager;
         this.view = new PlayerView(this);
     }
+    /*public static void printBoard(BoardCell[][] board) {
+        int numRows = board.length;
+        int numCols = board[0].length;
+        int i=0;
+
+        // Print column headers
+        System.out.print("  ");
+        for (int j = 0; j < numCols; j++) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        for (BoardCell[] row : board) {
+            System.out.print(i + " ");
+            i++;
+            for (BoardCell cell : row) {
+                if(cell.getStatus.equals("IN")){
+                    switch (cell.getTile().getType()) {
+                        case CATS -> System.out.print("C ");
+                        case BOOKS -> System.out.print("B ");
+                        case GAMES -> System.out.print("G ");
+                        case FRAMES -> System.out.print("F ");
+                        case TROPHIES -> System.out.print("T ");
+                        case PLANTS -> System.out.print("P ");
+                        default -> System.out.print("*" + " ");
+                    }
+                }else{
+                    System.out.print("#" + " ");
+
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printBookshelf(ItemTile[][] bookshelf) {
+        // Print column headers
+        System.out.print("  ");
+        for (int j = 0; j < numCols; j++) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        for (ItemTile[] row : bookshelf) {
+            System.out.print("|");
+            for (ItemTile itemtile : row) {
+                switch (itemtile.getType()) {
+                    case CATS -> System.out.print("C" + "|");
+                    case BOOKS -> System.out.print("B" + "|");
+                    case GAMES -> System.out.print("G" + "|");
+                    case FRAMES -> System.out.print("F" + "|");
+                    case TROPHIES -> System.out.print("T" + "|");
+                    case PLANTS -> System.out.print("P" + "|");
+                    default -> System.out.print(" " + "|");
+                }
+            }
+            System.out.println();
+        }
+    }*/
 }
---
+++ MyShelfie/src/main/java/Messages/fromServerToClient/BoardToPrint.java
@@ -0,0 +1,13 @@
+package Messages.fromServerToClient;
+
+import Messages.Message;
+
+public class BoardToPrint extends Message {
+    //private String message;
+    /**
+     * Overview: Message constructor
+     */
+    public BoardToPrint() {
+        super(null);
+    }
+}

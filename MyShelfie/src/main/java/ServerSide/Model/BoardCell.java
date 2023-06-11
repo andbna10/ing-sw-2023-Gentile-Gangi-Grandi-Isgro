af50e9911fafc9494bae10fb7aacf61cd4acc8e8
andbna10 <andrea.isgro@mail.polimi.it>
2023-06-12 00:50:05 +0200

    starting doc at server>model>BoardCell

--- MyShelfie/src/main/java/ServerSide/Model/BoardCell.java
+++ MyShelfie/src/main/java/ServerSide/Model/BoardCell.java
@@ -1,13 +1,18 @@
 package ServerSide.Model;
 
+/**
+ * Overview: BoardCell class
+ * @author: Andrea Isgrò
+ */
 public class BoardCell {
     private Status status;
-
     private boolean pickable;
     private ItemTile tile;
 
     /**
-     * Overview: constructor 1 of boardcell
+     * Overview: BoardCell constructor
+     * @author Andrea Isgrò
+     * @param status indicates whether the cell is fillable with a tile or not during the game
      */
     public BoardCell(Status status){
         pickable = false;
@@ -15,27 +20,37 @@ public class BoardCell {
     }
 
     /**
-     * Overview: get status
+     * Overview: status getter
+     * @author Andrea Isgrò
+     * @return the status of the BoardCell
      */
     public Status getStatus(){ return status; }
 
     /**
      * Overview: get tile
+     * @author Andrea Isgrò
+     * @return the tile associated to the cell
      */
     public ItemTile getTile(){ return tile; }
 
     /**
-     * Overview: set tile
+     * Overview: tile setter
+     * @author Andrea Isgrò
+     * @param tile ItemTile which will be associated to the cell
      */
     public void setTile(ItemTile tile){ this.tile = tile; }
 
     /**
      * Overview: pickable setter
+     * @author Andrea Isgrò
+     * @param arg boolean that marks the tile associated to the cell as pickable
      */
     public void setPickable(boolean arg) {pickable = arg;}
 
     /**
      * Overview: pickable getter
+     * @author Andrea Isgrò
+     * @return the pickable status
      */
     public Boolean getPickable(){ return this.pickable; }
 }
